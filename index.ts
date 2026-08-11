b977f34c58fda0b750d1571921697e46561f8819
Tanpaisen <tinh7demtb@gmail.com>
2026-08-11 11:09:37 +0700

    Khởi tạo ExpressJS sử dụng Typescript

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log('ok')
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello, World!");
+});
+
+const PORT: string | number = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
